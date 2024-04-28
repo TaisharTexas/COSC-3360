25063f9bb805238d0191be933d0a011aaed6b939
Andrew <bayledgeacademy@icloud.com>
2024-04-28 14:06:43 -0500

    working

--- hw3.cpp
+++ hw3.cpp
@@ -5,16 +5,26 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 int main() {
     // Open the file for reading
     ifstream infile("inputs/Assignment3_Input.txt");
 
-    // Variables to store the numbers
+    // Variables to store inputs
     int totalPageFrames, pageSize, framesPerProcess, lookaheadWindow, minPool, maxPool, numProcesses;
+    
+    // The unique ids for the processes that will be requesting addresses
     vector<int> processIds;
-    vector<int> processAddrs;
+    // the number of page frames on disk for each ID
+    vector<int> processSizes;
+    
+    // The queue is two vectors, one vector holds the ID of the process and the other holds the address being requested
+    // the id of the process making the request
+    vector<int> processIdQueue;
+    // the address being requested
+    vector<string> processAddrQueue;
 
     // Read each var from the file
     infile >> totalPageFrames >> pageSize >> framesPerProcess >> lookaheadWindow >> minPool >> maxPool >> numProcesses;
@@ -28,18 +38,39 @@ int main() {
     printf("Num Processes:      %d\n", numProcesses);
 
     
-    int tempId, tempAddr;
-    infile >> tempId >> tempAddr; // clean inputs
+    int tempId, tempSize;
     for(int i = 0; i< numProcesses; i++){
-        printf("Read: tempId: %d, tempAddr: %d\n", tempId, tempAddr);
-        infile >> tempId >> tempAddr;
+        infile >> tempId >> tempSize;
+        // printf("Read: tempId: %d, tempAddr: %d\n", tempId, tempSize);
         processIds.push_back(tempId);
-        processAddrs.push_back(tempAddr);
+        processSizes.push_back(tempSize);
+    }
+    cout << "collected processes" << endl;
+
+    cout << "ids: " << endl;
+    for(int i= 0; i<processIds.size(); i++){
+        cout << processIds.at(i) << endl;
+    }
+    cout << "sizes: "<< endl;
+    for(int i= 0; i<processSizes.size(); i++){
+        cout << processSizes.at(i) << endl;
+    }
+
+    //grab queue of requests
+    string eachAddr;
+    int eachId;
+    printf("Queue of process requests: \n");
+    while(infile >> eachId >> eachAddr){
+        processIdQueue.push_back(eachId);
+        processAddrQueue.push_back(eachAddr);
+        printf("Id: %d, Addr: %s\n", eachId, eachAddr.c_str());
     }
 
     // Close the file
     infile.close();
 
+
+    //
 
     return 0;
 }
