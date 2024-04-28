3ff52d735b7d601d30dfc169e05a4c6b8433bcdf
Andrew <bayledgeacademy@icloud.com>
2024-04-28 10:38:58 -0500

    working

--- hw3.cpp
+++ hw3.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -12,21 +13,32 @@ int main() {
 
     // Variables to store the numbers
     int totalPageFrames, pageSize, framesPerProcess, lookaheadWindow, minPool, maxPool, numProcesses;
+    vector<int> processIds;
+    vector<int> processAddrs;
 
-    // Read each number from the file
+    // Read each var from the file
     infile >> totalPageFrames >> pageSize >> framesPerProcess >> lookaheadWindow >> minPool >> maxPool >> numProcesses;
+
+    printf("Total Page Frames:  %d\n", totalPageFrames);
+    printf("Page Size:          %d\n", pageSize);
+    printf("Frames Per Process: %d\n", framesPerProcess);
+    printf("Lookahead Window:   %d\n", lookaheadWindow);
+    printf("Min Free Pool Size: %d\n", minPool);
+    printf("Max Free Pool Size: %d\n", maxPool);
+    printf("Num Processes:      %d\n", numProcesses);
+
     
+    int tempId, tempAddr;
+    infile >> tempId >> tempAddr; // clean inputs
+    for(int i = 0; i< numProcesses; i++){
+        printf("Read: tempId: %d, tempAddr: %d\n", tempId, tempAddr);
+        infile >> tempId >> tempAddr;
+        processIds.push_back(tempId);
+        processAddrs.push_back(tempAddr);
+    }
 
     // Close the file
     infile.close();
-
-    cout << totalPageFrames << endl;
-    cout << pageSize << endl;
-    cout << framesPerProcess << endl;
-    cout << lookaheadWindow << endl;
-    cout << minPool << endl;
-    cout << maxPool << endl;
-    cout << numProcesses << endl;
 
 
     return 0;
